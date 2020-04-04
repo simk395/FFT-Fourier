9d1452413db22e4d05fbefc93acb3537ffa5d6ca
Simon Kongm <simon.kong95@gmail.com>
2020-04-04 17:13:54 -0400

    figure out how to draw rectangle

--- main.js
+++ main.js
@@ -3,22 +3,30 @@ const canvas = document.getElementById('drop-area')
 const ctx = canvas.getContext("2d");
  //x y width height
 
-const handleMouseMove = e =>{
-    console.log(e)
-    ctx.strokeRect(e.clientX,e.clientY,e.clientX,e.clientY);
-}
-
-const handleMouseUp = e => {
-    canvas.removeEventListener("mousemove", handleMouseMove)
-}
-
 const handleMouseDown = e => {
     console.log(e)
-    canvas.addEventListener("mousemove", handleMouseMove)
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const handleMouseMove = (e, x = mouseX, y = mouseY) => {
+      console.log(e,mouseX, mouseY)
+      ctx.clearRect(0,0, 1280, 990); //width and height hardcoded
+      let width = 50,
+          height = 50;
+      ctx.lineWidth = 5;
+      ctx.strokeRect(e.clientX,e.clientY,width,height);
+    }
+    const handleMouseUp = e => {
+      canvas.removeEventListener("mousemove", handleMouseMove)
+    }
+    
+    canvas.addEventListener("mousemove",handleMouseMove )
     canvas.addEventListener("mouseup", handleMouseUp)
 }
 
 canvas.addEventListener('mousedown', handleMouseDown)
 
 
-  
+  // 1. click the mouse downwards
+  // 2. get x y coordinates
+  // 3. 
+  // 4. profit
